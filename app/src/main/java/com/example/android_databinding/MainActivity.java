4e98df39a4c47a3c85d3210e410cdcf9afc557fc
Karan <Kumarkay.ca@gmail.com>
2019-11-28 20:36:26 -0500

    init

--- app/src/main/java/com/example/android_databinding/MainActivity.java
+++ app/src/main/java/com/example/android_databinding/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.android_databinding.databinding.ActivityMainBinding;
@@ -11,16 +13,26 @@ import com.example.android_databinding.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     EditText nameEditTxt;
-
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        final ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
         nameEditTxt=findViewById(R.id.nameEditTxt);
         Person person = new Person("Hello");
         binding.setPerson(person);
-      //  setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.change);
+      btn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              String chng;
+              chng=nameEditTxt.getText().toString();
+              Person person1=new Person(chng);
+              binding.setPerson(person1);
+          }
+      });
+        //  setContentView(R.layout.activity_main);
 
 
 //        nameEditTxt=findViewById(R.id.nameEditTxt);
